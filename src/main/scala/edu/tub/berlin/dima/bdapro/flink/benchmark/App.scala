f2f5d1a23ae78f625f88dcf9e94d66b3007dd8e5
mujadid <hafiz.mujadid@cloudplex.io>
2019-02-25 07:24:57 +0100

    sc1

--- src/main/scala/edu/tub/berlin/dima/bdapro/flink/benchmark/App.scala
+++ src/main/scala/edu/tub/berlin/dima/bdapro/flink/benchmark/App.scala
@@ -16,12 +16,12 @@ object App {
     env.enableCheckpointing(JobConfig.CHECKPOINT_INTERVAL)
     env.setParallelism(22)
     env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, Time.of(60, TimeUnit.SECONDS) ))
-    val rocksDBStateBackend: RocksDBStateBackend = new RocksDBStateBackend(JobConfig.CHECKPOINT_DIR,
-      false)
+    //val rocksDBStateBackend: RocksDBStateBackend = new RocksDBStateBackend(JobConfig.CHECKPOINT_DIR,
+      //false)
     //rocksDBStateBackend.setPredefinedOptions(PredefinedOptions.SPINNING_DISK_OPTIMIZED_HIGH_MEM)
     //rocksDBStateBackend.setOptions(new RocksDbStateBackendOptions)
-    env.setStateBackend(rocksDBStateBackend)
-    //env.setStateBackend(new FsStateBackend(JobConfig.CHECKPOINT_DIR))
+    //env.setStateBackend(rocksDBStateBackend)
+    env.setStateBackend(new FsStateBackend(JobConfig.CHECKPOINT_DIR))
 
     val query = new MonitorNewUsers
     query.run(env)
