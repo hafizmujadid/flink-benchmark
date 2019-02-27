8187d3f055363fbe715a7479f12816cb388b4f38
mujadid <hafiz.mujadid@cloudplex.io>
2019-02-27 10:23:15 +0100

    tui

--- src/main/scala/edu/tub/berlin/dima/bdapro/flink/benchmark/Sorter.scala
+++ src/main/scala/edu/tub/berlin/dima/bdapro/flink/benchmark/Sorter.scala
@@ -2,29 +2,29 @@ package edu.tub.berlin.dima.bdapro.flink.benchmark
 
 object Sorter {
   def main(args: Array[String]): Unit = {
-    val prefix ="/home/mujadid/Documents/"
-    val throughput= scala.io.Source.fromFile(prefix+"throughput_inc_uniq_q6_tumbling.csv").getLines().filter(_.nonEmpty)
-    val eventTime = scala.io.Source.fromFile(prefix+"event_letency_inc_uniq_q6_tumbling.csv").getLines().filter(_.nonEmpty)
-    val processTime: Iterator[String] =scala.io.Source.fromFile(prefix+"process_letency_inc_uniq_q6_tumbling.csv").getLines().filter(_.nonEmpty)
+    val prefix ="/home/mujadid/Documents/tumbling_full/"
+    val throughput= scala.io.Source.fromFile(prefix+"tumbling_full_throughput_short.csv").getLines().filter(_.nonEmpty)
+    val eventTime = scala.io.Source.fromFile(prefix+"tumbling_full_event_short.csv").getLines().filter(_.nonEmpty)
+    val processTime: Iterator[String] =scala.io.Source.fromFile(prefix+"tumbling_full_process_short.csv").getLines().filter(_.nonEmpty)
 
     processLatency(eventTime,"EventTime")
     processLatency(processTime,"processTime")
 
-    /*val tuples = throughput.map(x=>{
+    val tuples = throughput.map(x=>{
       val t =x.split(",")
       (t(0).toLong,t(1).toInt)
     }).toList.sortBy(_._1)
     val avg_throughput = tuples.map(_._2).sum / tuples.size
-    print(tuples,avg_throughput,"throughput")*/
+    print(tuples,avg_throughput,"throughput")
   }
 
-  def print[A](tuples:List[A],avrg:Double, metric:String)={
+  private def print[A](tuples:List[A], avrg:Double, metric:String): Unit ={
     println("Printing "+metric+"  \n")
     println("Average "+metric +" value is "+avrg)
     tuples.foreach(x=>println(x))
   }
 
-  def processLatency(iter:Iterator[String],metric:String)={
+  private def processLatency(iter:Iterator[String], metric:String): Unit ={
     val eventLatency = iter.map(x=>{
       val t =x.split(",")
       (t(0).toLong,t(1).toDouble)
