bd40bc89ab606432a29f8fc256b54fb965e20b19
mujadid <hafiz.mujadid@cloudplex.io>
2019-03-09 08:19:44 +0100

    test checkpoint

--- src/main/scala/edu/tub/berlin/dima/bdapro/flink/benchmark/App.scala
+++ src/main/scala/edu/tub/berlin/dima/bdapro/flink/benchmark/App.scala
@@ -14,11 +14,11 @@ object App {
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
     env.enableCheckpointing(JobConfig.CHECKPOINT_INTERVAL)
     //env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, Time.of(60, TimeUnit.SECONDS) ))
-    val rocksDBStateBackend: RocksDBStateBackend = new RocksDBStateBackend(JobConfig.CHECKPOINT_DIR,
-      false)
+    /*val rocksDBStateBackend: RocksDBStateBackend = new RocksDBStateBackend(JobConfig.CHECKPOINT_DIR,
+      false)*/
     //rocksDBStateBackend.setPredefinedOptions(PredefinedOptions.SPINNING_DISK_OPTIMIZED_HIGH_MEM)
-    env.setStateBackend(rocksDBStateBackend)
-    //env.setStateBackend(new FsStateBackend(JobConfig.CHECKPOINT_DIR))
+    //env.setStateBackend(rocksDBStateBackend)
+    env.setStateBackend(new FsStateBackend(JobConfig.CHECKPOINT_DIR))
 
     val query = new WordCountStream
     query.run(env)
